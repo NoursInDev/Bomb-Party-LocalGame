aa1a13aa113092e6c02b4b6a10fd0d34200e7ee0
XolotlSouillou <willnot44@outlook.fr>
2023-04-18 00:00:41 +0200

    clarity game.py update
    added comments to increase the program's readability

--- game.py
+++ game.py
@@ -4,22 +4,22 @@ import random
 import unicodedata
 import json
 import threading
-from normalize import *         #normalize()
-from checkindico import *       #checkindico() 
-from alreadywrited import *     #wrote(), alreadywriten(), reset_mots_ecrits()
-from bombtimer import *
+from normalize import *                                                                 #import normalize()
+from checkindico import *                                                               #import checkindico() 
+from alreadywrited import *                                                             #import wrote(), alreadywriten(), reset_mots_ecrits()
+from bombtimer import *                                                                 #import countdown()
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
 # liste_mots = all the valid words (dictionnary) in the game
 with open('alpha1.1.json','r') as fichier_json:
     liste_mots = fichier_json.read()
-print("mots dans dico:", len(liste_mots)) # returns the number of words in the dictionary
+print("mots dans dico:", len(liste_mots))                                               # returns the number of words in the dictionary
 mots_verif = open("mots_a_verif.exe", "a")
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-minimumTime = 10
+minimumTime = 10                                                                        #variable declaration
 
 timing = False
 
@@ -32,28 +32,28 @@ t1 = threading.Thread(target=countdown, args=(minimumTime,))
 print('[console] début du tour')
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-mot_recherche = liste_mots[random.randint(0,len(liste_mots))]
+mot_recherche = liste_mots[random.randint(0,len(liste_mots))]                           #randomly select one word in the dictionary
 t1.start()
-print(mot_recherche)
+print(mot_recherche)                                                                    #the searched word (just temporary, for the tests)
 
-while timing != True and endTurn != True:
+while timing != True and endTurn != True:                                               #while the countdown and the turn aren't over 
     mot_choisi = normalize(input("valeur = "))
-    if timing == True:
+    if timing == True:                                                                  #check if the countdown is over (just in case)
         endTurn = True
         print('tour terminé par explosion de bombe')
-    elif timing != True:
+    elif timing != True:                                                                #recheck if the countdown is over (just in case(we're never sure of anything))
         print(timing)
-        if checkindico(mot_choisi) == True:
-            if alreadywrited(mot_choisi) == False:
+        if checkindico(mot_choisi) == True:                                             #check if the choosed word is in the dictionary
+            if alreadywrited(mot_choisi) == False:                                      #check if the choosed word was already written
                 print('mot correct et non écrit actuellement')
                 wrote(mot_choisi)
-                if mot_choisi == mot_recherche : 
+                if mot_choisi == mot_recherche :                                        #check if the choosed word is the searched word
                     endTurn = True
                     print("c'était bien le mot " + mot_recherche + "! Bravo!")
-            else:
+            else:                                                                       #if the word was already written
                 print('mot déja écrit!')
-        else:
+        else:                                                                           #if the word isn't in the dictionary
             print("ce mot n'est pas dans la base de données actuellement !")
-            mots_verif.write(mot_choisi + ", ")
-    else:
+            mots_verif.write(mot_choisi + ", ")                                         #append the "invalid word" in a manual checking to potentially append it in the dictionary
+    else:                                                                               #shouldn't happen.
         print('bug')
