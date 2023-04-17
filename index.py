25d35787795639916b74b619d93dd420ea76e268
NoursInDev <nours44.mc@gmail.com>
2023-04-17 17:27:46 +0200

    Update index.py
    goal -> stop while when timing = True

--- index.py
+++ index.py
@@ -29,35 +29,40 @@ global endTurn
 
 endTurn = False
 
-
-
-
 # -_-_-_-_-
 timing = False
-t1 = threading.Timer(random.randint(minimumTime, minimumTime+7),set_timing_true)
+t1 = threading.Thread(target=compte_a_rebours, args=(1,))
 t1.start()
 
 
-while timing == False:
+while timing == False and endTurn == False:
     mot_choisi = normalize(input("valeur = "))
     print("verif contenu variable mot_choisi : " + mot_choisi) #verif
-    if checkindico(mot_choisi) == True:         #si mot dans dico
-        if alreadywrited(mot_choisi) == False:  #et pas encore écrit
-            print('mot correct')
-            wrote(mot_choisi)
-            endTurn = True
-            break
-        elif alreadywrited(mot_choisi) == True: #mais déja écrit
-            print('mot déja écrit!')
+    if timing == True:
+        print('la bombe a déja explosé !')
+        break
+    elif timing == False:
+        if checkindico(mot_choisi) == True:         #si mot dans dico
+            if alreadywrited(mot_choisi) == False:  #et pas encore écrit
+                print('mot correct')
+                wrote(mot_choisi)
+                endTurn = True
+                break
+            elif alreadywrited(mot_choisi) == True: #mais déja écrit
+                print('mot déja écrit!')
+            else:
+                print('unless you are modifying the script, this is a bug! please report it to the spark team') #bug report, ne devrais pas arriver
+                endTurn = True
+                break
+        elif checkindico(mot_choisi) == False:
+            print("mot non existant dans la base de donnée actuellement")           #mot qui n'est pas dans le dictionnaire =>> à créer : fonction stocker mots faux pour les rajouter ensuite dans dico
         else:
-            print('unless you are modifying the script, this is a bug! please report it to the spark team') #bug report, ne devrais pas arriver
+            print('unless you are modifying the script, this is a bug! please report it to the Spark Team')     #bug report, ne devrais pas arriver
             endTurn = True
             break
-    elif checkindico(mot_choisi) == False:
-        print("mot non existant dans la base de donnée actuellement")           #mot qui n'est pas dans le dictionnaire =>> à créer : fonction stocker mots faux pour les rajouter ensuite dans dico
+        pass
     else:
         print('unless you are modifying the script, this is a bug! please report it to the Spark Team')     #bug report, ne devrais pas arriver
         endTurn = True
         break
-    pass
 print("compteur toujours en cours ? | ",t1.is_alive())
