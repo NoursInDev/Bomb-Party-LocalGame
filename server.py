8aa71b071e7effba0c43a50f913f24647ce610e8
XolotlSouillou <willnot44@outlook.fr>
2023-04-17 18:16:09 +0200

    server translation
    translated comments form french to english

--- server.py
+++ server.py
@@ -4,24 +4,23 @@ import random
 import unicodedata
 import json
 import threading
-from normalize import *         #normalize()
-from checkindico import *       #checkindico() 
-from alreadywrited import *     #wrote(), alreadywriten(), reset_mots_ecrits()
-from bombtimer import *
+from normalize import *         #import normalize()
+from checkindico import *       #import checkindico() 
+from alreadywrited import *     #import wrote(), alreadywriten() and reset_mots_ecrits()
+from bombtimer import *         #import compte_a_rebours()
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-#PARAMETRES
-    #Temps minimum à la bombe:
+#parameters
+    #minimum bomb's time:
 minimumTime = 5
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-# liste_mots = tous les mots entrables
-#              dans le jeu (mots valides)
+# liste_mots = all the valid words (dictionnary) in the game
 with open('alpha1.1.json','r') as fichier_json:
     liste_mots = fichier_json.read()
-print("mots dans dico:", len(liste_mots)) # renvois nombre_mots
+print("mots dans dico:", len(liste_mots)) # returns the number of words in the dictionary
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
@@ -37,31 +36,31 @@ t1.start()
 
 while timing == False and endTurn == False:
     mot_choisi = normalize(input("valeur = "))
-    print("verif contenu variable mot_choisi : " + mot_choisi) #verif
+    print("verif contenu variable mot_choisi : " + mot_choisi) #print the choosed word for a manual check
     if timing == True:
         print('la bombe a déja explosé !')
         break
     elif timing == False:
-        if checkindico(mot_choisi) == True:         #si mot dans dico
-            if alreadywrited(mot_choisi) == False:  #et pas encore écrit
+        if checkindico(mot_choisi) == True:         #check if the choosed word is in the dictionnary 
+            if alreadywrited(mot_choisi) == False:  #and if it wasn't already writen
                 print('mot correct')
                 wrote(mot_choisi)
                 endTurn = True
                 break
-            elif alreadywrited(mot_choisi) == True: #mais déja écrit
+            elif alreadywrited(mot_choisi) == True: #if the choosed word is in the dictionnary but was already written
                 print('mot déja écrit!')
             else:
-                print('unless you are modifying the script, this is a bug! please report it to the spark team') #bug report, ne devrais pas arriver
+                print('unless you are modifying the script, this is a bug! please report it to the spark team') #bug report, should not happen
                 endTurn = True
                 break
-        elif checkindico(mot_choisi) == False:
+        elif checkindico(mot_choisi) == False:      #if the choosed word isn't in the dictionnary
             print("mot non existant dans la base de donnée actuellement")           #mot qui n'est pas dans le dictionnaire =>> à créer : fonction stocker mots faux pour les rajouter ensuite dans dico
         else:
-            print('unless you are modifying the script, this is a bug! please report it to the Spark Team')     #bug report, ne devrais pas arriver
+            print('unless you are modifying the script, this is a bug! please report it to the Spark Team')     #bug report, should not happen
             endTurn = True
             break
         pass
     else:
-        print('unless you are modifying the script, this is a bug! please report it to the Spark Team')     #bug report, ne devrais pas arriver
+        print('unless you are modifying the script, this is a bug! please report it to the Spark Team')     #bug report, should not happen
         endTurn = True
         break
