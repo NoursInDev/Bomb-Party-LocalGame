b33f9ab9751bcfab0180c9cbeb19956b2f6dc2b0
NoursInDev <nours44.mc@gmail.com>
2023-04-17 19:30:07 +0200

    Update game.py

--- game.py
+++ game.py
@@ -10,3 +10,40 @@ from alreadywrited import *     #wrote(), alreadywriten(), reset_mots_ecrits()
 from bombtimer import *
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+# liste_mots = all the valid words (dictionnary) in the game
+with open('alpha1.1.json','r') as fichier_json:
+    liste_mots = fichier_json.read()
+print("mots dans dico:", len(liste_mots)) # returns the number of words in the dictionary
+
+# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+minimumTime = 5
+
+timing = False
+
+endTurn = False
+
+# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+t1 = threading.Thread(target=countdown, args=(minimumTime),)
+
+print('[console] début du tour')
+
+# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+t1.start()
+
+while timing != True and endTurn != True:
+    mot_choisi = normalize(input("valeur = "))
+    print('contenu mot_choisi: ', mot_choisi)
+    if timing == True:
+        endTurn = True
+        print('tour terminé par explosion de bombe')
+        break
+    else:
+        if checkindico(mot_choisi) == True:
+            pass
+        else:
+            print("ce mot n'est pas dans la base de données actuellement !")
+            
