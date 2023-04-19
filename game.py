390a211d5197d6beb62aba162233c1ec523fcfa5
NoursInDev <nours44.mc@gmail.com>
2023-04-19 11:06:34 +0200

    Syllable ADD
    Add of the traitementsyllabes.py file to create the list of syllables.
    Creation of the syllabes.json file (by the precedent file).
    Update of the game.py file by including the syllable requirement, and setting the turn as a function.

--- game.py
+++ game.py
@@ -19,43 +19,54 @@ mots_verif = open("mots_a_verif.txt", "a")
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-minimumTime = 3                  #variable declaration
+minimumTime = 3         #variable declaration
+gameDifficulty = 500    #1 = min 1 word/syllable in data base
 
 timing = False
 
 endTurn = False
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+def gameTurn(wordbysyllable):                                                                   #GAME TURN FUNCTION -> BASIC FUNCTION OF ALL THE GAME
+    bombTimer = random.randint(minimumTime, minimumTime+7)                                          #Duration generation
+    t1 = threading.Timer(bombTimer, lambda: print('timing bomb initial : ', bombTimer))             #Set Timer in a thread
 
-bombTimer = random.randint(minimumTime, minimumTime+7)
-t1 = threading.Timer(bombTimer, lambda: print('timing bomb initial : ', bombTimer))
-
-print('[console] début du tour')
-
-# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-t1.start()                                                                              #the searched word (just temporary, for the tests)
-
-while t1.is_alive() == True and endTurn != True:                                        #while the countdown and the turn aren't over 
-    mot_choisi = normalize(input("valeur = "))
-    if t1.is_alive() == False:                                                          #check if the countdown is over (just in case)
-        endTurn = True
-        print('tour terminé par explosion de bombe')
-    elif t1.is_alive() == True:                                                         #recheck if the countdown is over (just in case(we're never sure of anything))
-        if checkindico(mot_choisi) == True:                                             #check if the choosed word is in the dictionary
-            if alreadywrited(mot_choisi) == False:                                      #check if the choosed word was already written
-                print('mot correct et non écrit actuellement')
-                wrote(mot_choisi)
-                endTurn = True
-                break
-            else:                                                                       #if the word was already written
-                print('mot déja écrit!')
-        else:                                                                           #if the word isn't in the dictionary
-            print("ce mot n'est pas dans la base de données actuellement !")
-            mots_verif.write(mot_choisi + ", ")                                         #append the "invalid word" in a manual checking to potentially append it in the dictionary
-    else:                                                                               #shouldn't happen.
-        print('bug')
-        endTurn = True
-        break
-t1.join()
-print('verif : etat thread timer: ',t1.is_alive())
-mots_ecrits = []
+    print('[console] début du tour')                                                                #Console MSG
+    
+    with open('syllabes.json') as f:                                                                #Definition of the syllable
+        data = json.load(f)                                                                             #By opening the file
+    syllabe_list = [syllabe for syllabe, count in data.items() if count >= wordbysyllable]              #And selecting a random syllable with more or as many words per syllable as wordinsyllable
+    random_syllabe = random.choice(syllabe_list)
+    
+    print('syllabe: ', random_syllabe)                                                              #Print the syllable
+    # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    
+    t1.start()                                                                                      #Start the Thread
+    
+    while t1.is_alive() == True and endTurn != True:                                            #while the countdown and the turn aren't over 
+        mot_choisi = normalize(input("valeur = "))
+        if t1.is_alive() == False:                                                              #check if the countdown is over (just in case)
+            endTurn = True
+            print('tour terminé par explosion de bombe')
+        elif t1.is_alive() == True:                                                             #recheck if the countdown is over (just in case(we're never sure of anything))
+            if random_syllabe in mot_choisi:                                                    #check if the syllable is contained in the chosen word
+                if checkindico(mot_choisi) == True:                                             #check if the choosed word is in the dictionary
+                    if alreadywrited(mot_choisi) == False:                                      #check if the choosed word was already written
+                        print('mot correct et non écrit actuellement')
+                        wrote(mot_choisi)
+                        endTurn = True
+                        break
+                    else:                                                                       #if the word was already written
+                        print('mot déja écrit!')
+                else:                                                                           #if the word isn't in the dictionary
+                    print("ce mot n'est pas dans la base de données actuellement !")
+                    mots_verif.write(mot_choisi + ", ")                                         #append the "invalid word" in a manual checking to potentially append it in the dictionary
+            else:
+                print("ce mot ne contient pas la syllabe !")
+        else:                                                                               #shouldn't happen.
+            print('bug')
+            endTurn = True
+            break
+    t1.join()
+    print('verif : etat thread timer: ',t1.is_alive())
+    mots_ecrits = []
---
+++ traitementsyllabes.py
@@ -0,0 +1,25 @@
+import json
+from collections import Counter
+
+with open("alpha1.1.json", "r") as f:
+    words = json.load(f)
+
+letters_2 = Counter()
+letters_3 = Counter()
+
+for word in words:
+    for i in range(len(word) - 1):
+        letters_2[word[i:i+2]] += 1
+    for i in range(len(word) - 2):
+        letters_3[word[i:i+3]] += 1
+
+syllables = {}
+for key, value in letters_2.items():
+    if value >= 100:
+        syllables[key] = value
+for key, value in letters_3.items():
+    if value >= 100:
+        syllables[key] = value
+
+with open("syllabes.json", "w") as f:
+    json.dump(syllables, f)
